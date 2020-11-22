88d9a2c6ba6f280a642fd231e4646176e74a9c6d
kousueke <k.koko.koko.kousuke@gmail.com>
2020-11-22 23:43:45 +0900

    ユーザー認証機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,35 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  def destroy
+    
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:,)
+  end
+
+
 end
