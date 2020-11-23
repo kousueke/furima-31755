4296495f06397fd3c15297f2baee19c79126df09
kousueke <k.koko.koko.kousuke@gmail.com>
2020-11-23 21:35:41 +0900

    Revert "Merge pull request #2 from kousueke/ユーザー管理機能"
    This reverts commit 09e2e3b0c89d4e8682a2c20e023cbca32a6ff416, reversing
    changes made to ffbb752223346085264513b107b5c3d7eb597c5d.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,37 +2,5 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    # Item.create(item_params)
-  end
-
-  def destroy
-  end
-
-  def edit
-  end
-
-  # def update
-  # if current_user.update(user_params)
-  # redirect_to root_path
-  # else
-  # render :edit
-  # end
-  # end
-
-  def show
-  end
-
-  # def item_params
-  # params.require(:item).permit(:,)
-  # end
-
-  # def user_params
-  # params.require(:user).permit(:nickname, :firstname, :lastname, :firstname_kana, :lastname_kana, :bithday)
-  # end
+  
 end
