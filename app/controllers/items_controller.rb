eb87a45e8f3ea4a0b34cdac5d7ad6c3c041a680b
kousueke <k.koko.koko.kousuke@gmail.com>
2020-11-26 11:05:12 +0900

    ログイン状態の選別

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,11 @@ class ItemsController < ApplicationController
   def create
      Item.create.(item_params)
      #@item = Item.new(item_params)
+     if @item.save
+      redirect_to root_path
+     else
+       render :new
+     end
   end
 
   def destroy
