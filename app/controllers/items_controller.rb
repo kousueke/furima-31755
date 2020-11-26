8347dee56e129b859a97e16a3af2fcfa738e0f6f
kousueke <k.koko.koko.kousuke@gmail.com>
2020-11-26 18:04:52 +0900

    ログイン状態の選別の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :redirect_root, except: :new
+  before_action :authenticate_user!, except: :index
+
+
   def index
     @items = Item.all
   end
@@ -6,10 +10,12 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+  #- ログイン状態のユーザーだけが、商品出品ページへ遷移できること
+  #- ログアウト状態のユーザーは、商品出品ページへ遷移しようとすると、ログインページへ遷移すること
 
   def create
-     Item.create.(item_params)
-     #@item = Item.new(item_params)
+     #Item.create.(item_params)
+     @item = Item.new(item_params)
      if @item.save
       redirect_to root_path
      else
@@ -34,8 +40,18 @@ class ItemsController < ApplicationController
   def show
   end
 
+  private
+
    def item_params
-   params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :deli_price_id, :area_id, :deli_day_id, :price)#.merge(user_id: current_user.id)
+   params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :deli_price_id, :area_id, :deli_day_id, :price).merge(user_id: current_user.id)
    end
+
+   def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+  def redirect_root
+    redirect_to user_sessions_path unless user_signed_in?
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -16,7 +16,7 @@ class Item < ApplicationRecord
   end
 
   with_options  numericality: { other_than: 1 } do
-    validates :categiry_id
+    validates :category_id
     validates :status_id
     validates :deli_price_id
     validates :area_id
