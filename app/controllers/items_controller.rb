449525d238d935d93c845a7ac77750d3e928ae7b
kousueke <k.koko.koko.kousuke@gmail.com>
2020-12-02 13:07:24 +0900

    不要なコードの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :update, :show, :destroy]
-
+  before_action :set_item2, only: [:edit, :destroy]
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
   end
@@ -20,13 +20,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path unless current_user.id == @item.user_id
     @item.destroy
     redirect_to root_path
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -54,5 +52,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def set_item2
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
