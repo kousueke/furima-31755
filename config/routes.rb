f811b7525e5f4e11390a1b98ab6296a3f12b0257
kousueke <k.koko.koko.kousuke@gmail.com>
2020-12-10 11:09:00 +0900

    コメント機能ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'comments/new'
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:new, :create, :destroy, :edit, :update, :show] do
     resources :buys, only: [:index, :create]
+  root 'messages#new'
+    resources :comments, only: [:create]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2120_12_03_035856) do
+ActiveRecord::Schema.define(version: 2120_12_03_035857) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 2120_12_03_035856) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["item_id"], name: "index_buys_on_item_id"
     t.index ["user_id"], name: "index_buys_on_user_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
